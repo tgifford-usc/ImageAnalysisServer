69e7fb0b1003b7987ce43eaba4709584529f1ec1
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2024-10-14 21:08:46 +1000

    Fixed error in cors and trying to parse html as json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
@@ -12,8 +13,8 @@ app.use(cors())
 app.get('/', async (req, res) => {
   const queryURL = "https://www.google.com/search?q=banana";
   const response = await fetch(queryURL);
-  const result = await response.json();
-  console.log(result);
+  //const result = await response.json();
+  console.log(response);
   res.json({ description: "A man with glasses" });
 })
 
