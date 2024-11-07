2745cc38e81be45b17b9abf9da5adc8216002b40
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2024-11-07 14:20:33 +1000

    Changed color back

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function analyseImage(dataURL) {
   const body = {
     contents : [{
       parts: [
-          { text: "What is the main color of this image?"},
+          { text: "What is this picture?"},
           { inline_data: {
               mime_type: "image/png",
               data: base64Data
