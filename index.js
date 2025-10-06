e0e412efca20fae0d79c59ba98befabfcb78d3d5
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2025-10-06 21:04:31 +1000

    Changed back to png images

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function analyseImage(dataURL) {
       parts: [
           { text: "What is this picture?"},
           { inline_data: {
-              mime_type: "image/jpeg",
+              mime_type: "image/png",
               data: base64Data
             }}
           ]}
