98cb91643d20488f96aaf7130b87560fe1898731
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2025-10-08 19:23:40 +1000

    Changed question back

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function analyseImage(dataURL) {
   const body = {
     contents : [{
       parts: [
-          { text: "Is there a threat in this image? If so, describe it in detail. If not, say 'No threat detected'." },
+          { text: "What is this picture?"},
           { inline_data: {
               mime_type: "image/png",
               data: base64Data
